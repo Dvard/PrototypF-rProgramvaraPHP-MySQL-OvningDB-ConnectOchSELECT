0952423f41c5a2d8de5320bd70f2ed006a1c76cc
Edvard DHG19 <56823476+Dvard@users.noreply.github.com>
2020-09-28 14:09:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,20 @@ include('db_connect.php');
 $q_select = 'SELECT * FROM students WHERE start_year BETWEEN 2006 AND 2009';
 $stmt = $conn->query($q_select);
 
+?>
 
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo $row['firstname'] . ' ' . $row['lastname'] . '<br>';
-}
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>PHP Övning</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+        <p>
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo $row['firstname'] . ' ' . $row['lastname'] . '<br>';
+            }
+        </p>
+	</body>
+</html>
